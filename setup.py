f21821bd880202f3fce94e3118c7d69c23b33707
piushvaish <vaishpiush@gmail.com>
2020-03-15 06:41:36 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A comprehensensive overview of multiple algorithms used in time series',
+    author='Piush Vaish',
+    license='MIT',
+)
